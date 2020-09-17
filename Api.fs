ac72313864c48b09c364471f3f9d7e96d3e750d4
Adam Davidson <adam.davidson@emulate3d.com>
2020-09-17 11:59:29 +0100

    first commit

---
+++ Api.fs
@@ -0,0 +1,39 @@
+module Api
+
+open FSharp.Data
+open System
+
+type Daily = CsvProvider<"../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/03-22-2020.csv">
+
+let files = 
+    System.IO.Directory.GetFiles("../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports", "*.csv")
+    |> Seq.filter (fun filename -> System.IO.Path.GetFileNameWithoutExtension(filename).StartsWith("03"))
+    |> Seq.map System.IO.Path.GetFullPath
+
+let allData = 
+    files 
+    |> Seq.map Daily.Load
+    |> Seq.collect (fun data -> data.Rows)
+    |> Seq.distinctBy (fun row -> row.Country_Region, row.Province_State, row.Last_Update.Date)
+    |> Seq.sortBy (fun row -> row.Last_Update.Date)
+    |> Seq.filter (fun row -> row.Country_Region <> "Others")
+    |> Seq.toArray
+
+let cleanseCountry country = 
+    match country with
+    | "Unite States" -> "US"
+    | _ -> country
+
+let confirmedbyCountryDaily =
+    [| for country, rows in allData |> Seq.groupBy (fun r -> cleanseCountry r.Country_Region) do
+        let countryData = 
+            [| for date, rows in rows |> Seq.groupBy (fun r -> r.Last_Update.Date) do
+                {| Date = date
+                   Confirmed = rows |> Seq.sumBy (fun r-> r.Confirmed) |}
+            |]
+        country, countryData
+    |]
+
+let countryLookup = confirmedbyCountryDaily |> Map
+
+let allCountries = confirmedbyCountryDaily |> Array.map fst
---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿open Saturn
+open Giraffe
+
+module ApiRoutes =
+    let findByCountry country =
+        match Api.countryLookup.TryFind country with
+        | Some stats -> json stats
+        | None -> RequestErrors.NOT_FOUND (sprintf "Unknown country %s" country)
+
+    let apiRouter = router {
+        get "/api/countries" (Api.allCountries |> json)
+        getf "/api/countries/%s" findByCountry 
+    }
+
+module UiRoutes =
+    let uiRouter = text "Hello World"
+
+let appRouter = router {
+    forward "/api" ApiRoutes.apiRouter
+    forward "" UiRoutes.uiRouter
+}
+
+let myApplication = application {
+    use_json_serializer (Thoth.Json.Giraffe.ThothSerializer())
+    use_router appRouter
+}
+
+run myApplication
+
+(*
+    http://localhost:5000/
+    http://localhost:5000/api/countries
+    http://localhost:5000/api/countries/China
+    http://localhost:5000/api/countries/UnknownCountry
+*)
