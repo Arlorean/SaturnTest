c9155c123d6f8c0b44b5c96395c59910bbb351db
Adam Davidson <adam.davidson@emulate3d.com>
2020-09-18 13:51:04 +0100

    Added table layout and Bulma CSS and country pages

--- Api.fs
+++ Api.fs
@@ -29,7 +29,9 @@ let confirmedbyCountryDaily =
         let countryData = 
             [| for date, rows in rows |> Seq.groupBy (fun r -> r.Last_Update.Date) do
                 {| Date = date
-                   Confirmed = rows |> Seq.sumBy (fun r-> r.Confirmed) |}
+                   Confirmed = rows |> Seq.sumBy (fun r-> r.Confirmed) 
+                   Deaths = rows |> Seq.sumBy (fun r-> r.Deaths) 
+                   Recovered = rows |> Seq.sumBy (fun r-> r.Recovered) |}
             |]
         country, countryData
     |]
@@ -37,3 +39,11 @@ let confirmedbyCountryDaily =
 let countryLookup = confirmedbyCountryDaily |> Map
 
 let allCountries = confirmedbyCountryDaily |> Array.map fst
+
+let countryStats = [|
+    for country, stats in confirmedbyCountryDaily do
+        let mostRecent = stats |> Array.tryLast
+        match mostRecent with
+        | Some mostRecent -> {| mostRecent with Country=country |}
+        | None -> ()
+|]
--- Program.fs
+++ Program.fs
@@ -8,12 +8,81 @@ module ApiRoutes =
         | None -> RequestErrors.NOT_FOUND (sprintf "Unknown country %s" country)
 
     let apiRouter = router {
-        get "/api/countries" (Api.allCountries |> json)
-        getf "/api/countries/%s" findByCountry 
+        get "/countries" (Api.allCountries |> json)
+        getf "/countries/%s" findByCountry 
     }
 
 module UiRoutes =
-    let uiRouter = text "Hello World"
+    open GiraffeViewEngine
+    open Zanaptak.TypedCssClasses
+
+    [<Literal>]
+    let BulmaUrl = "https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css"
+    type Bulma = CssClasses<BulmaUrl>
+
+    let _classes attributes = attributes |> String.concat " " |> _class
+
+    let createPage title subtitle contents =
+        html [] [
+            head [] [
+                link [ _rel "stylesheet"; _href BulmaUrl ]
+            ]
+            body [] [
+                section [ _classes [ Bulma.hero; Bulma.``is-primary`` ] ] [
+                    div [ _class Bulma.``hero-body`` ] [
+                        div [ _class Bulma.container ] [
+                            h1 [ _class Bulma.title ] [ Text title ]
+                            h2 [ _class Bulma.subtitle ] [ Text subtitle ]
+                        ]
+                    ]
+                ]
+                section [ _class Bulma.section ] [
+                    div [ _class Bulma.container]
+                        contents
+                ]
+            ]
+        ]
+
+    let countriesView = 
+        createPage "COVID 19 Dataset" "Written in F#" [
+            table [ _class Bulma.table ] [
+                thead [] [
+                    tr [] [
+                        th [] [ Text "Country" ]
+                        th [] [ Text "Deaths" ]
+                        th [] [ Text "Recovered" ]
+                        th [] [ Text "Confirmed" ]
+                    ]
+                ]
+                tbody [] [
+                    for row in Api.countryStats |> Array.sortByDescending (fun row -> row.Deaths) do
+                        tr [] [
+                            td [] [ a [ _href (sprintf "/%s" row.Country ) ] [Text row.Country ] ]
+                            td [] [ Text (row.Deaths.ToString "N0") ]
+                            td [] [ Text (row.Recovered.ToString "N0") ]
+                            td [] [ Text (row.Confirmed.ToString "N0") ]
+                        ]
+                ]
+            ]
+        ]
+
+    let countryView country =
+        createPage country "Deaths over time" [
+            match Api.countryLookup.TryFind country with
+            | Some stats ->
+                let chart =
+                    stats
+                    |> Array.map (fun s -> s.Date, s.Deaths)
+                    |> XPlot.Plotly.Chart.Line
+                chart.GetHtml() |> Text
+            | None ->
+                Text "No data found for country!"
+        ]
+
+    let uiRouter = router {
+        get "/" (htmlView countriesView)
+        getf "/%s" (countryView >> htmlView)
+    }
 
 let appRouter = router {
     forward "/api" ApiRoutes.apiRouter
